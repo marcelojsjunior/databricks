45f9de1888e0738ccfeb1ce94c986afa2a53fcaf
marcelojsjunior <marcelojsjunior>
2025-06-19 22:36:13 +0000

    Aplicativo de analise streamlit

--- cripto_db/load/streamlit_app.py
+++ cripto_db/load/streamlit_app.py
@@ -6,7 +6,7 @@ url = "https://raw.githubusercontent.com/marcelojsjunior/databricks/main/transac
 df = pd.read_csv(url)
 df["data"] = pd.to_datetime(df["data"])
 
-st.title("📊 Volume de Tokens negociados em DEX")
+st.title("📊 Top 20 - Métricas de Tokens negociados em DEX por dia")
 
 top_tokens = (
     df.groupby("nome_token")["total_trades"]
@@ -17,22 +17,30 @@ top_tokens = (
     .tolist()
 )
 
-
 df_top = df[df["nome_token"].isin(top_tokens)]
 
-
-token = st.selectbox("Selecione o token", sorted(df_top["nome_token"].unique()))
+token = st.selectbox("🔎 Selecione o token", sorted(df_top["nome_token"].unique()))
+metrica = st.selectbox("📈 Selecione a métrica", {
+    "Volume em USD": "volume_total_usd",
+    "Total de Trades": "total_trades",
+    "Média de Tokens por Transação": "media_quantia_token"
+})
 
 df_filtrado = df_top[df_top["nome_token"] == token].sort_values("data")
+
 
 fig = px.line(
     df_filtrado,
     x="data",
-    y="volume_total_usd",
+    y=metrica,
     color="direcao",
     markers=True,
-    title=f"Volume por dia – {token}",
+    title=f"{metrica.replace('_', ' ').capitalize()} por dia – {token}",
     template="plotly_white"
 )
-st.plotly_chart(fig, use_container_width=True)
 
+
+fig.update_xaxes(nticks=len(df_filtrado["data"].unique()))
+
+
+st.plotly_chart(fig, use_container_width=True)
